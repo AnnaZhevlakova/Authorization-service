79d9f370fcea854364315d561137687741a48514
Anna Zhevlakova <azevlakova12@gmail.com>
2025-08-20 21:38:55 +0400

    1

---
+++ src/main/java/Authorization/service/Authorization/service/Models/UserEntity.java
@@ -0,0 +1,41 @@
+package Authorization.service.Authorization.service.Models;
+
+import Authorization.service.Authorization.service.Enum.Authorities;
+
+import java.util.List;
+
+public class UserEntity {
+    private String login;
+    private String password;
+    private List<Authorities> rights;
+
+    public UserEntity(String login, String password, List<Authorities> rights){
+        this.login = login;
+        this.password = password;
+        this.rights = rights;
+    }
+
+    public String getLogin(){
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Authorities> getRights() {
+        return rights;
+    }
+
+    public void setRights(List<Authorities> rights) {
+        this.rights = rights;
+    }
+}
--- src/main/java/Authorization/service/Authorization/service/Repository/UserRepository.java
+++ src/main/java/Authorization/service/Authorization/service/Repository/UserRepository.java
@@ -1,13 +1,39 @@
 package Authorization.service.Authorization.service.Repository;
 
 import Authorization.service.Authorization.service.Enum.Authorities;
+
+import Authorization.service.Authorization.service.Models.UserEntity;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Repository
 public class UserRepository {
+    private static List<UserEntity> list = new CopyOnWriteArrayList<>(
+            List.of(
+                    new UserEntity(
+                            "admin",
+                            "admin123",
+                            Arrays.asList(Authorities.READ, Authorities.WRITE, Authorities.DELETE)),
+                    new UserEntity(
+                            "user1",
+                            "user1",
+                            Arrays.asList(Authorities.READ)),
+                    new UserEntity(
+                            "user2",
+                            "user2",
+                            Arrays.asList(Authorities.WRITE, Authorities.DELETE))));
+
+
     public List<Authorities> getUserAuthorities(String user, String password) {
-        return null;
+        var result = list.stream()
+                .filter(x -> x.getLogin().equals(user) && x.getPassword().equals(password))
+                .map(UserEntity::getRights)
+                .findFirst()
+                .orElse(new ArrayList<>());
+        return result;
     }
 }
